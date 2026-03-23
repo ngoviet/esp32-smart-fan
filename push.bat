0773cc0318a9c59258b7c43a81e799e0021085e1
Ngo Viet <ngoducvietds@gmail.com>
2026-03-23 18:02:35 +0700

    Fix batch syntax error in flash.bat

--- push.bat
+++
@@ -1,6 +0,0 @@
-git config user.name "Ngo Viet"
-git config user.email "ngoducvietds@gmail.com"
-git add .
-git commit -m "Initial commit: ESP32 BLDC Fan Controller với EC11"
-"C:\Program Files\GitHub CLI\gh.exe" repo create esp32-smart-fan --public --source=. --remote=origin --push
-pause
